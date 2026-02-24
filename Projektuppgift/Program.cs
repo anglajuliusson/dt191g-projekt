d2add35d5ed887a42162d6368351941fb6a0ad6a
Ängla Juliusson <angla02@icloud.com>
2026-02-24 20:41:18 +0100

    Lägg till ApplicationDbContext och konfigurera SQLite i Program.cs

--- Projektuppgift/Program.cs
+++ Projektuppgift/Program.cs
@@ -1,4 +1,14 @@
+using Projektuppgift.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews(); // Aktivera MVC
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultDbString"))
+);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
