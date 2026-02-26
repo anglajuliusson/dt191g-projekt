10f4b8a0320acc6925283677ed8a96cedf006dfb
Ängla Juliusson <angla02@icloud.com>
2026-02-26 14:21:58 +0100

    Gör navigations properties nullable

--- Projektuppgift/Models/Booking.cs
+++ Projektuppgift/Models/Booking.cs
@@ -5,10 +5,10 @@ public class Booking
     public int BookingId { get; set; }
 
     public int FacilityId { get; set; }
-    public Facility Facility { get; set; } = null!;
+    public Facility? Facility { get; set; }
 
     public int AssociationId { get; set; }
-    public Association Association { get; set; } = null!;
+    public Association? Association { get; set; }
 
     public DateTime StartDateTime { get; set; }
     public DateTime EndDateTime { get; set; }
