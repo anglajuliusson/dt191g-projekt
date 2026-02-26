0c44f95c1c74d22de0239e716836ba0d34728a94
Ängla Juliusson <angla02@icloud.com>
2026-02-26 15:22:10 +0100

    Korrekt utskrift av datum och tid

--- Projektuppgift/Models/Booking.cs
+++ Projektuppgift/Models/Booking.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Projektuppgift.Models;
 
 public class Booking
@@ -10,7 +12,10 @@ public class Booking
     public int AssociationId { get; set; }
     public Association? Association { get; set; }
 
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
     public DateTime StartDateTime { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
     public DateTime EndDateTime { get; set; }
 
     public string? Status { get; set; }
