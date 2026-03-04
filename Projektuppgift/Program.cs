29078b70a45523f8b75e6d6d67b8fe8e27373991
Ängla Juliusson <angla02@icloud.com>
2026-03-04 11:07:48 +0100

    Konfigurera Identity i Program.cs

--- Projektuppgift/Program.cs
+++ Projektuppgift/Program.cs
@@ -1,5 +1,6 @@
 using Projektuppgift.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +10,15 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultDbString"))
 );
 
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 var app = builder.Build();
 
 app.UseStaticFiles(); // Användning av statiska filer
 app.UseRouting(); // Användning av routing
+
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
