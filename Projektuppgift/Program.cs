a0de0f3cf1f7d21417f933a1323f4024db1216b5
Ängla Juliusson <angla02@icloud.com>
2026-03-04 11:10:40 +0100

    Ändra från true till false

--- Projektuppgift/Program.cs
+++ Projektuppgift/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultDbString"))
 );
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
